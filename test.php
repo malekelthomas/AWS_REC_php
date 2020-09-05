5c2924c0847cd52760b02842f826958c35f0d307
maleke <malekelthomas97@gmail.com>
2020-09-05 04:14:55 -0400

    Table made for categories selected

--- test.php
+++ test.php
@@ -11,64 +11,16 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <script src="test.js"></script>
-    <!-- <script> 
-      $(document).ready(function () {
-        var checkboxes = $("input[type=checkbox]");
-        var z="";
-        console.dir(checkboxes);
-        checkboxes.change(function(){
-          if (this.checked){
-            if (z.includes(this.value) == false){
-              z+=this.value+"<br>";
-              $("#btnData").html(z);
-              console.log(z);
-            }
-          }
-          if (this.checked == false){
-            if (z.includes(this.value)){
-              z= z.replace(this.value+"<br>", "");
-              $("#btnData").html(z);
-              console.log(z);
-              }
-            }
-          })
-        })
-    </script> -->
-   <!--  <script>
-          
-          $(document).ready(function(){
-            
-            var numCatButtons = 5;
-            $("#btn").on('click',function(){
-              var btnClickValues = JSON.parse(localStorage.getItem('btnClickValues')) || {};
-              var $checkboxes = $("#myForm :checkbox");
-              console.log($checkboxes)
-              numCatButtons+=3;
-              $checkboxes.each(function(){
-              btnClickValues[this.id] = this.checked;
-              console.log(this)
-            });
-            localStorage.setItem("btnClickValues", JSON.stringify(btnClickValues));
-            $.each(btnClickValues, function(key, value) {
-            console.log("Key:",key,"Value:", value)
-            $("#" + key).prop('checked', value);
-          });  
-            $("#categories").load("load_categories.php", {
-                newNumCatButtons: numCatButtons
-              })
-            })
-          })
-    </script> -->
-     
   </head>
+  <style>
+  </style>
   <body>
 
   <title>Test</title> 
     <h1>Categories</h1>
-    <div id = categories>
-      <form id = "myForm">
+    <div class = "categories" id = "categories">
         <?php
-          $sql = "SELECT * FROM cat LIMIT 5";
+          $sql = "SELECT * FROM cat LIMIT 0,5";
           $result = mysqli_query($conn, $sql);
           if(mysqli_num_rows($result) > 0){
             while ($row = mysqli_fetch_assoc($result)){?>
@@ -88,17 +40,28 @@
               echo "No categories";
           }
         ?>
-        <input type="hidden" id ="yerr" name="yerr" value="">
-      </form>
     </div>
-
+    <div id = addCategories>
+    </div>
     <button id = "btn">See More</button>
-    <button id="submitBtn">Submit</button>
     
-    <p id="btnData">
-      Data
+    <div id=categoryTable>
+    <form id = "categories-selected-form" action="get_products.php" method="POST">
+      <div class = "selected">
+            <table class = "table" id = "selected-cats">
+              <thead>
+                <tr>
+                  <th>Selected Categories</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+      </div>
+    </form>
+    <button id="submitBtn" type ="button" onclick="submitCategories(event);">Submit</button>
+    </div>
     </p>
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
