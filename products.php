055fa6591b44700d1493d0deaea1affdde97877a
maleke <malekelthomas97@gmail.com>
2020-09-09 02:56:24 -0400

    Created array for images

--- products.php
+++ products.php
@@ -3,18 +3,28 @@
     //echo var_dump($_POST);
     $data = json_encode($post_data);
     $data = json_decode($data, true);
+    $img_arr = array();
+    $i = 0;
     foreach($data as $key => $val){
         foreach($val as $descriptions => $descriptor){
             if (is_array($descriptor)){
                 foreach($descriptor as $details){
                     foreach($details as $stuff){
                         if(strpos($stuff, ".jpg") !== false){
-                            echo "<img src=$stuff>";
+                            //echo "<img src=$stuff>";
+                            $img_arr[$i] = $stuff;
+                            $i++;
                         }
                     }
                 }
             }
         }
     }
-
+    echo var_dump($img_arr);
 ?>
+
+<script>
+
+
+
+</script>
