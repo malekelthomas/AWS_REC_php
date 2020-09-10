5a57a374e960e1831983ee0733ab420c60b24894
maleke <malekelthomas97@gmail.com>
2020-09-10 02:38:08 -0400

    Store image links in array and store in session

--- products.php
+++ products.php
@@ -1,4 +1,7 @@
+
+
 <?php
+    session_start();
     $post_data = $_POST;
     //echo var_dump($_POST);
     $data = json_encode($post_data);
@@ -9,22 +12,23 @@
         foreach($val as $descriptions => $descriptor){
             if (is_array($descriptor)){
                 foreach($descriptor as $details){
-                    foreach($details as $stuff){
-                        if(strpos($stuff, ".jpg") !== false){
-                            //echo "<img src=$stuff>";
-                            $img_arr[$i] = $stuff;
-                            $i++;
+                    foreach($details as $product_picture){
+                        if(strpos($product_picture, ".jpg") !== false){
+                            if (is_array($product_picture) == false){
+                                //echo "$key,$stuff\n";
+                                if ($img_arr["$key"] == null){
+                                    $img_arr["$key"] = array();
+                                    }
+                                $img_arr["$key"][$i] = $product_picture;
+                                $i++;
+                            }
                         }
                     }
                 }
             }
         }
     }
-    echo var_dump($img_arr);
+    $_SESSION["images"] = $img_arr;
 ?>
 
-<script>
 
-
-
-</script>
