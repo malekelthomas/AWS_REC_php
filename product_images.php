89ff4a7091efefcc4fbf8ccb2701c2fb0dc383f0
maleke <malekelthomas97@gmail.com>
2020-09-12 19:44:12 -0400

    Input value includes product url

--- product_images.php
+++ product_images.php
@@ -12,9 +12,12 @@
 
             if($_SERVER["REQUEST_METHOD"] == "GET"){
                 foreach($_SESSION["images"] as $categories => $products){
-                    foreach($products as $images){
-                        echo "<img class='rounded-circle' value ='$categories' src=$images>";?>
-                        <input type="hidden" name ="products[]" value ='<?php echo "$images";?>'/><?php
+                    foreach($products as $details){
+                        //details = [product-url,product-img]
+                        $links = preg_split("/[,]/", $details);
+                        echo "<img class='rounded-circle' value ='{$links[0]}' src={$links[1]}>";
+                        //echo "$categories,{$links[0]}, {$links[1]}\n";?>
+                        <input type="hidden" name="products[]" value='<?php echo "{$links[0]} {$links[1]}"?>'/><?php
                     }
                 };
             }
